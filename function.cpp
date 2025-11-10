0f6085b940723636b3909d0d35891934f0caf009
Abdullah Alam Ansari <abdullahalamofc57@gmail.com>
2025-11-10 19:59:25 +0500

    Add files via upload

--- function.cpp
+++ function.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a = 0;
-    cout<<a;
-}
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int array [5] = {10,20,30,40,50};
+//     int reverse_array[5];
+//     // for(int i = 0;i < 5;i++)
+//     // {
+//     //     cout<<"Enter the elements to be in an array: ";
+//     //     cin>>array[i];
+//     // }
+//     int end_index = 4;
+//     for(int k=0;k<5;k++)
+//     {
+//         int temp;
+//         if(end_index)
+
+//             temp = array[k];
+//             reverse_array[k] = array[end_index];
+//             reverse_array[end_index] = array[k];
+
+//             cout<<array[k];
+//             end_index--;
+            
+        
+        
+//     }
+    
+// }
